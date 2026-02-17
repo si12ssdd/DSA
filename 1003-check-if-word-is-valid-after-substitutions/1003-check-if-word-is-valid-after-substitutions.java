9f103cebdeb71648c74bc981e03a0c7a199987d3
Siddharth <96983765+si12ssdd@users.noreply.github.com>
2026-02-17 14:39:14 +0530

    Time: 25 ms (26.24%), Space: 46.8 MB (19.86%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -1,30 +1,17 @@
-public class Solution {
+class Solution {
     public boolean isValid(String s) {
-
-      /*  while (s.contains("abc")) {
-            s = s.replace("abc", "");
-        }
-
-        return s.length() == 0; */
-
-
-        
-
-
         Stack<Character> stack = new Stack<>();
 
-        for (char ch : s.toCharArray()) {
+        for(char ch : s.toCharArray()){
             stack.push(ch);
 
-            // Whenever stack size >= 3, check top 3 characters
-            if (stack.size() >= 3) {
+
+            if(stack.size()>=3){
                 int size = stack.size();
 
-                if (stack.get(size - 3) == 'a' &&
-                    stack.get(size - 2) == 'b' &&
-                    stack.get(size - 1) == 'c') {
-
-                    // Remove "abc"
+                if(stack.get(size - 3) == 'a' &&
+                stack.get(size - 2) == 'b' && 
+                stack.get(size -1) == 'c'){
                     stack.pop();
                     stack.pop();
                     stack.pop();
@@ -35,5 +22,3 @@ public class Solution {
         return stack.isEmpty();
     }
 }
-
-
