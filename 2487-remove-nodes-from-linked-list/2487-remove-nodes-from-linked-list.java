7d75c034824c97355d12e26914f9585654b8369b
Siddharth <96983765+si12ssdd@users.noreply.github.com>
2026-07-05 20:55:55 +0530

    Time: 6 ms (92.11%), Space: 130.6 MB (62.23%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -11,39 +11,37 @@
 class Solution {
     public ListNode removeNodes(ListNode head) {
 
-        // Reverse the linked list
         head = reverse(head);
 
         ListNode curr = head;
         int max = curr.val;
 
-        while (curr != null && curr.next != null) {
-
-            if (curr.next.val < max) {
-                // Remove the next node
+        while( curr != null && curr.next != null){
+            if(curr.next.val < max){
                 curr.next = curr.next.next;
-            } else {
-                // Update maximum and move forward
-                curr = curr.next;
+
+            } else{
+                curr = curr.next; 
                 max = curr.val;
             }
         }
-
-        // Reverse again to restore order
         return reverse(head);
+
+    
+
     }
 
-    private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
 
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        private ListNode reverse(ListNode head){
+            ListNode prev = null;
+            ListNode curr = head;
+            while(curr != null ){
+                ListNode next = curr.next; 
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
         }
-
-        return prev;
+        
     }
-}
